c799e654d6cf33983f62f4d1f7c413b1192c7ca7
Abe G <abenbg12@gmail.com>
2025-04-04 20:33:51 -0400

    Works for all changes from 0 to 25 cents, except 14...

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ change = float(input("Enter change owed (in $, not cents!!): "))
 #Convert the change into an integer (i.e $0.25 becomes 25 cents)
 change = change * 100
 
-array_changes = [5, 10]
+array_changes = [5, 10, 15, 20]
 arr_size = len(array_changes)
 
 array = [0, 1, 2, 3, 4]
 length = len(array)
+
 
 
 #WHen change owed is a nickel, dime or quarter
@@ -24,16 +25,35 @@ if change in range(5):
     print(f"\nYou will carry: {number} coins \n\nTotal breakdown: 1 cent * {amount}\n")
 
 #when change is between 5 and 15
-if change in range(5,15,1):
+if change in range(5,25,1):
     for i in range(arr_size):
         for j in range(length):
             if change - array[j] == array_changes[i]:
-                #number of coins to carry
-                number = array[j] + 1
-                print(f"\nYou will carry: {number} coins\n")
-                # total breakdown
-                array[j] + array_changes[i]
-                if array[j] == 0:
-                    print(f"Total breakdown: {array_changes[i]} cents * 1")
+                if array_changes[i] == 15:
+                    #number of coins to carry
+                    number = array[j] + 2
+                    print(f"\nYou will carry: {number} coins\n")
+                    # total breakdown
+                    if array[j] == 0:
+                        print(f"Total breakdown: {array_changes[1]} cents * 1, {array_changes[0]} cents * 1 \n")
+                    else:
+                        print(f"Total breakdown: {array_changes[1]} cents * 1, {array_changes[0]} cents * 1, 1 cent * {array[j]} \n")
+                elif array_changes == 20:
+                    #number of coins to carry
+                    number = array[j] + 2
+                    print(f"\nYou will carry: {number} coins\n")
+                    # total breakdown
+                    if array[j] == 0:
+                        print(f"Total breakdown: {array_changes[1]} cents * 2\n")
+                    else:
+                        print(f"Total breakdown: {array_changes[1]} cents * 2, {array_changes[0]} cents * 1, 1 cent * {array[j]} \n")
                 else:
-                    print(f"Total breakdown: {array_changes[i]} cents * 1, {array[j]} cents * 1 \n")
+
+                    #number of coins to carry
+                    number = array[j] + 1
+                    print(f"\nYou will carry: {number} coins\n")
+                    #  total breakdown
+                    if array[j] == 0:
+                        print(f"Total breakdown: {array_changes[i]} cents * 1")
+                    else:
+                        print(f"Total breakdown: {array_changes[i]} cents * 1, 1 cent * {array[j]} \n")
